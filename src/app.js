6f14d343d8e6ec29f5f2f78d1108fa6c419fdc10
ttsang2111 <ttsang2111@gmail.com>
2023-11-03 11:52:56 +0700

    handle errors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const helmet = require('helmet');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
 const app = express();
 
 // init middlewares
 app.use(helmet())
 app.use(morgan("dev"))
+app.use(compression())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
@@ -14,5 +16,20 @@ require('./dbs/init.mongodb.js')
 
 // init routes
 app.use(require('./routes/index.js'))
+
+// error handling middlewares called
+app.use((req, res, next) => {
+    const err = new Error("Not Found!")
+    err.status = 404
+    next(err)
+})
+
+// middleware error handler 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        status: error.status || 500,
+        message: error.message || "Internal Server Error."
+    })
+})
 
 module.exports = app

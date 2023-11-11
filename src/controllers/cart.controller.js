bd24fb2d4aac1dc5b2fc76c81f9db7fbab4b842f
ttsang2111 <ttsang2111@gmail.com>
2023-11-12 02:54:43 +0700

    fix cart controller

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -25,21 +25,21 @@ class CartController {
         new SuccessResponse({
             message: "Update cart successfully",
             metadata: await CartService.addToCartV2( req.body )
-        })
+        }).send(res)
     }
 
     delete = async (req, res, next) => {
         new SuccessResponse({
             message: "Delete cart successfully",
             metadata: await CartService.deleteUserCart( req.body )
-        })
+        }).send(res)
     }
 
     listToCart = async (req, res, next) => {
         new SuccessResponse({
             message: "List cart successfully",
             metadata: await CartService.listToCart( req.body )
-        })
+        }).send(res)
     }
 }
 

f739fe6d919729e304cc78fcbf6ad7c08098e8ab
Truong Tuan Sang <sangchtk2001@gmail.com>
2023-12-02 02:21:37 +0700

    fix: add constraints to comment schema

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -6,12 +6,12 @@ const DOCUMENT_NAME = "Comment";
 const COLLECTION_NAME = "Comments";
 
 const commentSchema = new Schema({
-    comment_productId: { type: Schema.Types.ObjectId, ref: "Product" },
-    comment_userId: { type: Schema.Types.ObjectId, ref: "Shop" },
-    comment_content: { type: String, default: 'text' },
-    comment_left: { type: Number, default: 0 }, 
-    comment_right: { type: Number, default: 0 },
-    comment_parentId: { type: Schema.Types.ObjectId, ref: DOCUMENT_NAME },
+    comment_productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+    comment_userId: { type: Schema.Types.ObjectId, ref: "Shop", required: true },
+    comment_content: { type: String, default: 'text', required: true },
+    comment_left: { type: Number, default: 0, required: true }, 
+    comment_right: { type: Number, default: 0, required: true },
+    comment_parentId: { type: Schema.Types.ObjectId, ref: DOCUMENT_NAME, required: true },
     isDeleted: { type: Boolean, default: false }
 }, {
     timestamps: true,
